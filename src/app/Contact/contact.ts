2b097cb4524242d9c9c73421ce671a09e8c5012a
osailer <osailer13@gmail.com>
2025-04-16 22:29:31 -0500

    Auto-generated commit

--- src/app/Contact/contact.ts
+++
@@ -1,7 +0,0 @@
-export interface Contact {
-  id: number;  
-  first_name: string;
-  last_name: string;
-  phone_number: string;
-  email_address: string;
-}
--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ContactListComponent } from './components/contact-list/contact-list.component';
-import { ContactFormComponent } from './components/contact-form/contact-form.component';
-
-export const routes: Routes = [
-  { path: '', component: ContactListComponent },
-  { path: '', component: ContactListComponent },
-  { path: 'contact-form', component: ContactFormComponent }
-];
